319bdc5d2745d5fbc3b892f7233eebc8b8adc67c
Trần Mạnh Cường <89560029+TranManhCuongQN@users.noreply.github.com>
2023-06-23 01:21:18 +0700

    update url

--- src/api/axiosClient.tsx
+++ src/api/axiosClient.tsx
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import queryString from 'query-string'
 
-const baseURL = 'http://localhost:4000/api/v1/'
+// const baseURL = 'http://localhost:4000/api/v1/'
+const baseURL = 'https://note-be-ten.vercel.app/api/v1/'
 const axiosClient = axios.create({
   baseURL,
   headers: {
